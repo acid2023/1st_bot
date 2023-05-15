4cf73481ad2ef6039e43284a2ca8ad9af1d24324
Sergey Kuzmin <s.kuzmin@mail.ru>
2023-05-15 08:53:48 +0800

    update on stoping bot

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import logging
 import settings
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from datetime import datetime
-import pytz
+import pytz, os
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -25,14 +25,25 @@ def tell_the_time(update, context):
     update.message.reply_text(f'My time is {_time.strftime("%H:%M:%S")}')
     update.message.reply_text(f'Moscow time is {moscow_time.strftime("%H:%M:%S")}')
 
+def stop_bot(update, context):
+    user_name = update.message.from_user.username
+    if user_name == "system_halted" :
+        os._exit(1)
+    else:   
+        update.message.reply_text(user_name + ' - нет прав на выключение' )
+    return
 
+def stopping(update, context):
+    user_name = update.message.from_user.username
+    update.message.reply_text(user_name + ' меня выключил' )
+    return stop_bot(update, context)
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
     dp = mybot.dispatcher   
     dp.add_handler(CommandHandler("start",greet_user))
     dp.add_handler(CommandHandler("time",tell_the_time))
-    dp.add_handler(CommandHandler('exit', quit()))
+    dp.add_handler(CommandHandler('exit', stopping))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info("Бот стратовал")
     mybot.start_polling()
